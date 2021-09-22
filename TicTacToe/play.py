386246709ce5ba67b971ef25693b72d91426c4aa
niksingh710 <nik.singh710@gmail.com>
2021-09-22 20:03:34 +0530

    Added Super computer
    Added Super Computer class in player.py

--- TicTacToe/play.py
+++ TicTacToe/play.py
@@ -1,5 +1,5 @@
-import sys
-from player import HumanPlayer, RandomComputerPlayer
+import argparse
+from player import HumanPlayer, RandomComputerPlayer, SuperComputerPlayer
 from tictactoe import TicTacToe
 
 __author__ = 'Nikhil Singh'
@@ -29,7 +29,41 @@ def play(game, x_player, o_player, verbose=False):
         print("It's a tie.")
 
 
+def test(n=10, verbose=False):
+    x, o, t = 0, 0, 0
+    for _ in range(n):
+        x_player, o_player = RandomComputerPlayer(
+            "X"), SuperComputerPlayer("O")
+        game = TicTacToe()
+        win = play(game, x_player, o_player, verbose)
+        if win == 'X':
+            x += 1
+        elif win == 'O':
+            o += 1
+        else:
+            t += 1
+    print(f"Total Games played       : {n}")
+    print(f"X wins (Super Computer)  : {o}")
+    print(f"O wins (Random Computer) : {x}")
+    print(f"Times it tied            : {t}")
+
+
 if __name__ == '__main__':
-    x_player, o_player = HumanPlayer("X"), RandomComputerPlayer("O")
-    game = TicTacToe()
-    play(game, x_player, o_player, True)
+    parser = argparse.ArgumentParser(description='Common Options')
+    parser.add_argument('-v', '--verbose', dest='verbose',
+                        help='Put the game in verbose mode', action='store_true',)
+    parser.add_argument('-t', '--test', dest='times', type=int,
+                        help='Runs the test case with the given value')
+    parser.add_argument('-r', '--random', dest='random',
+                        help='Set the against player to Random Player', action='store_true')
+    args = parser.parse_args()
+
+    if args.times:
+        test(args.times, args.verbose)
+    else:
+        if not args.random:
+            x_player, o_player = HumanPlayer("X"), SuperComputerPlayer("O")
+        else:
+            x_player, o_player = HumanPlayer("X"), RandomComputerPlayer("O")
+        game = TicTacToe()
+        play(game, x_player, o_player, args.verbose)
--- TicTacToe/player.py
+++ TicTacToe/player.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 
 class Player:
@@ -39,3 +40,50 @@ class HumanPlayer(Player):
             except ValueError:
                 print("Invalid Input please Try again")
         return move
+
+
+class SuperComputerPlayer(Player):
+    # Also Inherits from the Player class. uses minimax algorithm to get the position.
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    # Test all the possibilities and the return the most optimal position to win
+    def get_move(self, game) -> int:
+        # return random value if the board is empty
+        if game.available_space_count() == 9:
+            return random.choice(game.available_space())
+        else:
+            return self.minmax(game, self.letter)['position']
+
+    def minmax(self, state, player) -> dict:
+        # assigning current player as the max_player and other the other letter
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else "X"
+
+        if state.winner == other_player:
+            return {'position': None, 'score': 1*(state.available_space_count()+1) if other_player == max_player else -1*(state.available_space_count()+1)}
+        elif not state.available_space():
+            return {'position': None, 'score': 0}
+
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}
+        else:
+            best = {'position': None, 'score': math.inf}
+
+        for possible_move in state.available_space():
+            state.make_move(possible_move, player)
+
+            sim_score = self.minmax(state, other_player)
+
+            state.board[possible_move] = ' '
+            state.winner = None
+            sim_score['position'] = possible_move
+
+            if (
+                player == max_player
+                and sim_score['score'] > best['score']
+                or player != max_player
+                and sim_score['score'] < best['score']
+            ):
+                best = sim_score
+        return best
