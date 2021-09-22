cc5e01deedfb7e8c33d7d7b2868fd9cb642afab9
niksingh710 <nik.singh710@gmail.com>
2021-09-22 11:45:04 +0530

    Base TicTacToe File
    This File Contains the class of tictactoe.
    with all the basic functionality required to play the game.:

---
+++ TicTacToe/player.py
@@ -0,0 +1,46 @@
+import random
+
+__author__ = 'Nikhil Singh'
+__email__ = 'nik.singh710@gmail.com'
+__github__ = 'https://github.com/niksingh710'
+__telegram__ = 'https://t.me/niksingh710'
+
+
+class Player:
+    # Base Class of Player Type takes the player Playing Input ['X','O'] and initializes it.
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    # This Class inherits from the Player class
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    # Base class function is overridden and in prameter the game instance is passed.
+    # Returns random value from the available spaces in the game board.
+    def get_move(self, game):
+        return random.choice(game.available_space())
+
+
+class HumanPlayer(Player):
+    # Also Inherits from the Player class. but asks value from the user.
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    # Till the input is not a integer and in range (0-8) it will ask ask input
+    def get_move(self, game) -> int:
+        valid_move = False
+        while not valid_move:
+            move = input("Enter your move in between (0-8): ")
+            try:
+                move = int(move)
+                if move not in game.available_space():
+                    raise ValueError
+                valid_move = True
+            except ValueError:
+                print("Invalid Input please Try again")
+        return move
---
+++ TicTacToe/tictactoe.py
@@ -0,0 +1,94 @@
+class TicTacToe:
+    # This class will be the instance of the game
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]
+        self.winner = None
+
+    # print the Default Board Index for user to map moves
+    @staticmethod
+    def default_board():
+        for row in [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]:
+            print(f" | {' | '.join(row)} | ")
+        '''
+        The output will look like this
+        | 0 | 1 | 2 |
+        | 3 | 4 | 5 |
+        | 6 | 7 | 8 |
+        '''
+
+    def __str__(self):
+        '''
+        The output will look like this
+        |   |   |   |
+        |   |   |   |
+        |   |   |   |
+        '''
+        # The String returned here will be displayed on the screen in when printing the instance of the class
+        return "".join(
+            f" | {' | '.join(row)} | \n"
+            for row in [
+                [self.board[i] for i in range(j * 3, (j + 1) * 3)]
+                for j in range(3)
+            ]
+        )
+
+    def available_space(self):
+        # Returning a list containing the available index to play
+        return [idx for idx, val in enumerate(self.board) if val == ' ']
+
+    def available_space_count(self):
+        # Returns the count of the available space in the board
+        return self.board.count(' ')
+
+    def make_move(self, pos, letter):
+        # Does to move on the board as the pos argument
+        if self.board[pos] == ' ':
+            self.board[pos] = ' '
+            if self.check_win(pos, letter):
+                self.winner = letter
+            return True
+        return False
+
+    def check_win(self, pos, letter):
+
+        # getting row index e.g if pos=4 4//3=1 hence we are in first row
+        row_idx = pos//3
+
+        # taking whole row in list [0,1,2
+        #                           3,4,5
+        #                           6,7,8]
+        # [1*3:(1+1)*3]=[3:6] = [3,4,5] list slice
+
+        row = self.board[row_idx*3:(row_idx+1)*3]
+        if all(spot == letter for spot in row):
+            return True
+
+        # getting column index e.g if pos=4 4%3=1 hence we are in first column
+        col_idx = pos % 3
+
+        # taking whole column in list [0,1,2
+        #                              3,4,5
+        #                              6,7,8]
+        # [col_idx+i*3] here i will be [0,1,2] for every itterattion
+        # col=[1+0*3,1+1*3,1+2*3]=[1,4,7]
+
+        col = [self.board[col_idx+i*3] for i in range(3)]
+        if all(spot == letter for spot in col):
+            return True
+
+        # if pos is even the move done by the player is at a diagnol
+
+        if pos % 2 == 0:
+            # Hard input of diagonal value
+            diagnol = [self.board[i] for i in [0, 4, 8]]
+            if all(spot == letter for spot in diagnol):
+                return True
+
+            diagnol = [self.board[i] for i in [2, 4, 6]]
+            if all(spot == letter for spot in diagnol):
+                return True
+        # If none true the the move was not a winning move
+        return False
+
+    def contains_space(self):
+        return ' ' in self.board
